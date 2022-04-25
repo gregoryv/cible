a1ba8a261b7b030e634ef1ca8745040f88f776d0
Gregory Vincic <g@7de.se>
2022-04-25 19:33:45 +0200

    Add types Area and Tile

--- world.go
+++ world.go
@@ -2,14 +2,37 @@ package cible
 
 func NewWorld() *World {
 	return &World{
-		atlas: make(map[int]Location),
+		atlas: make([]*Area, 0),
 	}
 }
 
 type World struct {
-	atlas map[int]Location
+	atlas []*Area
 }
 
-type Location struct {
-	id int // same as atlas[id]
+func NewArea() *Area {
+	return &Area{}
+}
+
+type Area struct {
+	id int
+
+	tiles []*Tile
+}
+
+func (me *Area) AddTile(t *Tile) {
+	t.Id = len(me.tiles)
+	me.tiles = append(me.tiles, t)
+}
+
+func (me *Area) Size() int { return len(me.tiles) }
+
+func NewTile() *Tile {
+	return &Tile{}
+}
+
+type Tile struct {
+	Id    int
+	Short []byte
+	Long  []byte
 }
--- world_test.go
+++ world_test.go
@@ -5,3 +5,21 @@ import "testing"
 func TestWorld(t *testing.T) {
 	_ = NewWorld()
 }
+
+func TestArea(t *testing.T) {
+	t.Run("defaults to empty", func(t *testing.T) {
+		if a := NewArea(); a.Size() != 0 {
+			t.Error("not empty, got:", a.Size())
+		}
+	})
+
+	t.Run("AddTile", func(t *testing.T) {
+		a := NewArea()
+		a.AddTile(NewTile())
+		a.AddTile(NewTile())
+		if a.Size() != 2 {
+			t.Fail()
+		}
+	})
+
+}
