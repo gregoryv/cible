6db8827a83e564baf6523af8892bba9e8f6cc662
Gregory Vincic <g@7de.se>
2022-04-26 12:20:24 +0200

    Move myCave func

--- game.go
+++ game.go
@@ -11,7 +11,7 @@ import (
 func NewGame() *Game {
 	return &Game{
 		events: make(chan Event),
-		Logger: logger.New(),
+		Logger: logger.Silent,
 	}
 }
 
@@ -52,13 +52,47 @@ func (me *Game) EventChan() chan<- Event {
 	return me.events
 }
 
+// ----------------------------------------
+
+func myCave() *Area {
+	t1 := &Tile{
+		Ident: "01",
+		Short: "Cave entrance",
+		Nav:   Nav{N: "02"},
+
+		Long: `Hidden behind bushes the opening is barely visible.`,
+		//
+	}
+
+	t2 := &Tile{
+		Ident: "02",
+		Short: "Fire room",
+		Nav:   Nav{E: "03", S: "01"},
+
+		Long: `A small streek of light comes in from a hole in the
+		ceiling. The entrance is a dark patch on the west wall, dryer
+		than the other walls.`,
+		//
+	}
+
+	t3 := &Tile{
+		Ident: "03",
+		Short: "Small area",
+		Nav:   Nav{W: "02"},
+	}
+
+	return &Area{
+		Title: "Cave of Indy",
+		Tiles: Tiles{t1, t2, t3},
+	}
+}
+
+// ----------------------------------------
 type Player struct {
 	Name
 }
 
 type Name string
-
-// gomerge src: world.go
 
 type Area struct {
 	Title
--- game_test.go
+++ game_test.go
@@ -9,33 +9,27 @@ import (
 func TestGame(t *testing.T) {
 	t.Run("can be cancelled", func(t *testing.T) {
 		g := NewGame()
-		g.Logger = t
 		ctx, cancel := context.WithCancel(context.Background())
 		go g.Run(ctx)
 		cancel()
 	})
 	t.Run("can be stopped", func(t *testing.T) {
 		g := NewGame()
-		g.Logger = t
 		go g.Run(context.Background())
 		g.EventChan() <- EventStopGame
 	})
 	t.Run("handles events", func(t *testing.T) {
 		g := NewGame()
-		g.Logger = t
 		go g.Run(context.Background())
 		g.EventChan() <- EventPing
 		g.EventChan() <- &EventMove{Direction: E}
 	})
 	t.Run("handles unknown events", func(t *testing.T) {
 		g := NewGame()
-		g.Logger = t
 		go g.Run(context.Background())
 		g.EventChan() <- &EventMove{Direction: Direction(-1)}
 	})
 }
-
-// gomerge src: world_test.go
 
 func Test_cave(t *testing.T) {
 	area := myCave()
@@ -43,41 +37,6 @@ func Test_cave(t *testing.T) {
 		t.Log(tile, tile.Nav)
 	}
 }
-
-func myCave() *Area {
-	t1 := &Tile{
-		Ident: "01",
-		Short: "Cave entrance",
-		Nav:   Nav{N: "02"},
-
-		Long: `Hidden behind bushes the opening is barely visible.`,
-		//
-	}
-
-	t2 := &Tile{
-		Ident: "02",
-		Short: "Fire room",
-		Nav:   Nav{E: "03", S: "01"},
-
-		Long: `A small streek of light comes in from a hole in the
-		ceiling. The entrance is a dark patch on the west wall, dryer
-		than the other walls.`,
-		//
-	}
-
-	t3 := &Tile{
-		Ident: "03",
-		Short: "Small area",
-		Nav:   Nav{W: "02"},
-	}
-
-	return &Area{
-		Title: "Cave of Indy",
-		Tiles: Tiles{t1, t2, t3},
-	}
-}
-
-// gomerge src: event_test.go
 
 func TestEventMove(t *testing.T) {
 	p := Player{
