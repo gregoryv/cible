c24d093c418c7377ec70014f2f65ee914ebe894b
Gregory Vincic <g@7de.se>
2022-04-26 11:47:45 +0200

    gomerge

--- game.go
+++ game.go
@@ -2,7 +2,9 @@ package cible
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strings"
 )
 
 func NewGame() *Game {
@@ -52,3 +54,87 @@ type Player struct {
 }
 
 type Name string
+
+// gomerge src: world.go
+
+type Area struct {
+	Title
+	Tiles
+}
+
+type Tiles []*Tile
+
+type Tile struct {
+	Ident
+	Short
+	Long
+	Nav
+}
+
+func (me *Tile) String() string {
+	return fmt.Sprintf("%s %s", me.Ident, me.Short)
+}
+
+type Nav [4]Ident
+
+func (me Nav) String() string {
+	var res []string
+	for d, id := range me {
+		if id != "" {
+			res = append(res, Direction(d).String()+":"+string(id))
+		}
+	}
+	return strings.Join(res, " ")
+}
+
+type Ident string
+type Short string
+type Long string
+type Title string
+
+// gomerge src: event.go
+
+type Event interface {
+	Event() string
+}
+
+const (
+	EventStopGame EventString = "stop game"
+	EventPing     EventString = "ping"
+)
+
+type EventString string
+
+func (me EventString) Event() string { return string(me) }
+
+type EventMove struct {
+	Player
+	Direction
+}
+
+func (me *EventMove) Event() string {
+	return fmt.Sprintf("%s moves %s", me.Player.Name, me.Direction)
+}
+
+// gomerge src: direction.go
+
+type Direction int
+
+const (
+	N Direction = iota
+	E
+	S
+	W
+)
+
+func (me Direction) Opposite() Direction {
+	switch me {
+	case N:
+		return S
+	case E:
+		return W
+	case W:
+		return E
+	}
+	return me
+}
--- game_test.go
+++ game_test.go
@@ -2,6 +2,7 @@ package cible
 
 import (
 	"context"
+	"strings"
 	"testing"
 )
 
@@ -28,4 +29,59 @@ func TestGame(t *testing.T) {
 		go g.Run(context.Background())
 		g.EventChan() <- &EventMove{Direction: Direction(-1)}
 	})
+}
+
+// gomerge src: world_test.go
+
+func Test_cave(t *testing.T) {
+	area := myCave()
+	for _, tile := range area.Tiles {
+		t.Log(tile, tile.Nav)
+	}
+}
+
+func myCave() *Area {
+	t1 := &Tile{
+		Ident: "01",
+		Short: "Cave entrance",
+		Nav:   Nav{N: "02"},
+
+		Long: `Hidden behind bushes the opening is barely visible.`,
+		//
+	}
+
+	t2 := &Tile{
+		Ident: "02",
+		Short: "Fire room",
+		Nav:   Nav{E: "03", S: "01"},
+
+		Long: `A small streek of light comes in from a hole in the
+		ceiling. The entrance is a dark patch on the west wall, dryer
+		than the other walls.`,
+		//
+	}
+
+	t3 := &Tile{
+		Ident: "03",
+		Short: "Small area",
+		Nav:   Nav{W: "02"},
+	}
+
+	return &Area{
+		Title: "Cave of Indy",
+		Tiles: Tiles{t1, t2, t3},
+	}
+}
+
+// gomerge src: event_test.go
+
+func TestEventMove(t *testing.T) {
+	p := Player{
+		Name: "John",
+	}
+	e := &EventMove{p, S}
+	got := e.Event()
+	if !strings.Contains(got, "John") {
+		t.Errorf("missing name: %q", got)
+	}
 }
