6c81502852be8cd2e0fdba84fe4e8997bfc3cf8f
Gregory Vincic <g@7de.se>
2022-04-26 21:15:48 +0200

    Add initial cibtel.TelnetServer

--- cmd/cibtel/cibtel.go
+++ cmd/cibtel/cibtel.go
@@ -1,6 +1,66 @@
 // Command cibtel provides telnet access to a cible game
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
 
+	"github.com/gregoryv/cmdline"
+	"github.com/gregoryv/logger"
+)
+
+func main() {
+	var (
+		cli  = cmdline.NewBasicParser()
+		bind = cli.Option("-b, --bind").String(":8089")
+	)
+	cli.Parse()
+	srv := &TelnetServer{
+		Logger: logger.New(),
+		Bind:   bind,
+	}
+	if err := srv.Run(); err != nil {
+		srv.Log(err)
+		os.Exit(1)
+	}
+}
+
+type TelnetServer struct {
+	logger.Logger
+	Bind string
+}
+
+func (me *TelnetServer) Run() error {
+	ln, err := net.Listen("tcp", me.Bind)
+	if err != nil {
+		return err
+	}
+	me.Log("listen on", me.Bind)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			me.Log(err)
+		}
+		go me.handleConnection(conn)
+	}
+}
+
+func (me *TelnetServer) handleConnection(conn net.Conn) {
+	me.Log("connect", conn.RemoteAddr())
+	p := make([]byte, 1024)
+	for {
+		n, err := conn.Read(p)
+		if err != nil {
+			me.Log(err)
+		}
+		cmd := bytes.TrimRight(p[:n], "\r\n")
+		fmt.Println(string(cmd))
+		switch string(cmd) {
+		case ":q", ":quit":
+			conn.Close()
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/gregoryv/cible
 
 go 1.18
 
-require github.com/gregoryv/logger v0.2.0
+require (
+	github.com/gregoryv/cmdline v0.12.0
+	github.com/gregoryv/logger v0.2.0
+)
+
+require github.com/gregoryv/nexus v0.5.1 // indirect
