255a5f2d82d515754e51621da3a76f70b144767e
Gregory Vincic <g@7de.se>
2022-04-27 07:34:05 +0200

    Move server to root

---
+++ clientsrv.go
@@ -0,0 +1,79 @@
+package cible
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net"
+	"strings"
+
+	"github.com/gregoryv/logger"
+)
+
+type Server struct {
+	logger.Logger
+	Bind string
+}
+
+func (me *Server) Run(ctx context.Context, g *Game) error {
+	ln, err := net.Listen("tcp", me.Bind)
+	if err != nil {
+		return err
+	}
+	me.Log("listen on", me.Bind)
+
+	c := make(chan net.Conn)
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				me.Log(err)
+				continue
+			}
+			c <- conn
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			me.Log("server stop")
+			return nil
+		case conn := <-c:
+			go me.handleConnection(conn, g)
+		}
+	}
+
+	return nil
+}
+
+func (me *Server) handleConnection(conn net.Conn, g *Game) {
+	defer func() {
+		_ = recover()
+		Trigger(g, Leave("x"))
+		conn.Close()
+	}()
+	me.Log("connect ", conn.RemoteAddr())
+	p := make([]byte, 1024)
+	for {
+		n, err := conn.Read(p)
+		if err != nil {
+			if err != io.EOF {
+				me.Log(err)
+			}
+			return
+		}
+		cmd := bytes.TrimRight(p[:n], "\r\n")
+		args := strings.Fields(string(cmd))
+		switch args[0] {
+		case ":q", ":quit":
+			return
+		case ":join":
+			Trigger(g, Join(Player{Name: Name(args[1])}))
+
+		case ":stop game":
+			Trigger(g, StopGame())
+			return
+		}
+	}
+}
--- cmd/cible/cible.go
+++ cmd/cible/cible.go
@@ -2,16 +2,13 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
-	"strings"
 
-	. "github.com/gregoryv/cible"
+	"github.com/gregoryv/cible"
 	"github.com/gregoryv/cmdline"
 	"github.com/gregoryv/logger"
 )
@@ -25,7 +22,7 @@ func main() {
 	cli.Parse()
 	l := logger.Wrap(log.New(os.Stderr, "", log.LstdFlags))
 	if srv {
-		g := NewGame()
+		g := cible.NewGame()
 		g.Logger = l
 
 		ctx, cancel := context.WithCancel(context.Background())
@@ -33,19 +30,17 @@ func main() {
 			if err := g.Run(ctx); err != nil {
 				g.Log(err)
 			}
-			cancel()
+			cancel() // when game stops, stop the server
 		}()
 
-		srv := &Server{
-			Logger: l,
-			Bind:   bind,
-		}
+		srv := &cible.Server{Logger: l, Bind: bind}
 		if err := srv.Run(ctx, g); err != nil {
 			srv.Log(err)
 			os.Exit(1)
 		}
 		os.Exit(0)
 	}
+
 	conn, err := net.Dial("tcp", bind)
 	if err != nil {
 		l.Log(err)
@@ -53,72 +48,4 @@ func main() {
 	// send command
 	// todo maybe use gobs?
 	fmt.Fprintf(conn, os.Getenv("USER")+" join")
-}
-
-type Server struct {
-	logger.Logger
-	Bind string
-}
-
-func (me *Server) Run(ctx context.Context, g *Game) error {
-	ln, err := net.Listen("tcp", me.Bind)
-	if err != nil {
-		return err
-	}
-	me.Log("listen on", me.Bind)
-
-	c := make(chan net.Conn)
-	go func() {
-		for {
-			conn, err := ln.Accept()
-			if err != nil {
-				me.Log(err)
-				continue
-			}
-			c <- conn
-		}
-	}()
-
-	for {
-		select {
-		case <-ctx.Done():
-			me.Log("server stop")
-			return nil
-		case conn := <-c:
-			go me.handleConnection(conn, g)
-		}
-	}
-
-	return nil
-}
-
-func (me *Server) handleConnection(conn net.Conn, g *Game) {
-	defer func() {
-		_ = recover()
-		Trigger(g, Leave("x"))
-		conn.Close()
-	}()
-	me.Log("connect ", conn.RemoteAddr())
-	p := make([]byte, 1024)
-	for {
-		n, err := conn.Read(p)
-		if err != nil {
-			if err != io.EOF {
-				me.Log(err)
-			}
-			return
-		}
-		cmd := bytes.TrimRight(p[:n], "\r\n")
-		args := strings.Fields(string(cmd))
-		switch args[0] {
-		case ":q", ":quit":
-			return
-		case ":join":
-			Trigger(g, Join(Player{Name: Name(args[1])}))
-
-		case ":stop game":
-			Trigger(g, StopGame())
-			return
-		}
-	}
 }
