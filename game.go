5812f543777ad2c124fb3ce3fc8a67b122806072
Gregory Vincic <g@7de.se>
2022-04-27 18:00:02 +0200

    Refactor

--- game.go
+++ game.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/gregoryv/logger"
 )
@@ -101,61 +100,6 @@ type Player struct {
 
 type Bot struct{}
 
-type World struct {
-	Areas
-}
-
-func (w *World) Area(id Ident) (*Area, error) {
-	for _, a := range w.Areas {
-		if a.Ident == id {
-			return a, nil
-		}
-	}
-	return nil, fmt.Errorf("area %q not found", id)
-}
-
-type Areas []*Area
-
-type Area struct {
-	Ident
-	Title
-	Tiles
-}
-
-func (a *Area) Tile(id Ident) (*Tile, error) {
-	for _, t := range a.Tiles {
-		if t.Ident == id {
-			return t, nil
-		}
-	}
-	return nil, fmt.Errorf("tile %q not found", id)
-}
-
-type Tiles []*Tile
-
-type Tile struct {
-	Ident
-	Short
-	Long
-	Nav
-}
-
-func (t *Tile) String() string {
-	return fmt.Sprintf("%s %s", t.Ident, t.Short)
-}
-
-type Nav [4]Ident
-
-func (n Nav) String() string {
-	var res []string
-	for d, id := range n {
-		if id != "" {
-			res = append(res, Direction(d).String()+":"+string(id))
-		}
-	}
-	return strings.Join(res, " ")
-}
-
 type Position struct {
 	Area Ident
 	Tile Ident
@@ -170,45 +114,3 @@ type Name string
 type Short string
 type Long string
 type Title string
-
-// ----------------------------------------
-
-func Earth() World {
-	return World{
-		Areas: Areas{myCave()},
-	}
-}
-
-func myCave() *Area {
-	t1 := &Tile{
-		Ident: "01",
-		Short: "Cave entrance",
-		Nav:   Nav{N: "02"},
-
-		Long: `Hidden behind bushes the opening is barely visible.`,
-		//
-	}
-
-	t2 := &Tile{
-		Ident: "02",
-		Short: "Fire room",
-		Nav:   Nav{E: "03", S: "01"},
-
-		Long: `A small streek of light comes in from a hole in the
-		ceiling. The entrance is a dark patch on the west wall, dryer
-		than the other walls.`,
-		//
-	}
-
-	t3 := &Tile{
-		Ident: "03",
-		Short: "Small area",
-		Nav:   Nav{W: "02"},
-	}
-
-	return &Area{
-		Ident: "a1",
-		Title: "Cave of Indy",
-		Tiles: Tiles{t1, t2, t3},
-	}
-}
---
+++ world.go
@@ -0,0 +1,101 @@
+package cible
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Earth() World {
+	return World{
+		Areas: Areas{myCave()},
+	}
+}
+
+func myCave() *Area {
+	t1 := &Tile{
+		Ident: "01",
+		Short: "Cave entrance",
+		Nav:   Nav{N: "02"},
+
+		Long: `Hidden behind bushes the opening is barely visible.`,
+		//
+	}
+
+	t2 := &Tile{
+		Ident: "02",
+		Short: "Fire room",
+		Nav:   Nav{E: "03", S: "01"},
+
+		Long: `A small streek of light comes in from a hole in the
+		ceiling. The entrance is a dark patch on the west wall, dryer
+		than the other walls.`,
+		//
+	}
+
+	t3 := &Tile{
+		Ident: "03",
+		Short: "Small area",
+		Nav:   Nav{W: "02"},
+	}
+
+	return &Area{
+		Ident: "a1",
+		Title: "Cave of Indy",
+		Tiles: Tiles{t1, t2, t3},
+	}
+}
+
+type World struct {
+	Areas
+}
+
+func (w *World) Area(id Ident) (*Area, error) {
+	for _, a := range w.Areas {
+		if a.Ident == id {
+			return a, nil
+		}
+	}
+	return nil, fmt.Errorf("area %q not found", id)
+}
+
+type Areas []*Area
+
+type Area struct {
+	Ident
+	Title
+	Tiles
+}
+
+func (a *Area) Tile(id Ident) (*Tile, error) {
+	for _, t := range a.Tiles {
+		if t.Ident == id {
+			return t, nil
+		}
+	}
+	return nil, fmt.Errorf("tile %q not found", id)
+}
+
+type Tiles []*Tile
+
+type Tile struct {
+	Ident
+	Short
+	Long
+	Nav
+}
+
+func (t *Tile) String() string {
+	return fmt.Sprintf("%s %s", t.Ident, t.Short)
+}
+
+type Nav [4]Ident
+
+func (n Nav) String() string {
+	var res []string
+	for d, id := range n {
+		if id != "" {
+			res = append(res, Direction(d).String()+":"+string(id))
+		}
+	}
+	return strings.Join(res, " ")
+}
