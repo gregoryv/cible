92da26e45bfbc21445c71c96893ffb68c93d8cc7
Gregory Vincic <g@7de.se>
2022-05-02 19:47:50 +0200

    Color other peoples messages

--- cmd/cible/cible.go
+++ cmd/cible/cible.go
@@ -11,6 +11,7 @@ import (
 	. "github.com/gregoryv/cible"
 	"github.com/gregoryv/cmdline"
 	"github.com/gregoryv/logger"
+	"github.com/gregoryv/vt100"
 )
 
 var mlog = logger.Wrap(log.Default())
@@ -45,6 +46,7 @@ func main() {
 		os.Exit(0)
 	}
 
+	os.Stdout.Write([]byte("\033c"))
 	os.Stdout.Write(logo)
 
 	// connect client
@@ -74,54 +76,70 @@ func main() {
 	m.Direction = S
 	c.Out <- NewMessage(m)
 
+	writePrompt := func() { fmt.Printf("%s> ", cid) }
+	playerInput := make(chan string, 1)
 	go func() {
 		for {
-			select {
-			case m := <-c.In:
-				e, known := NewNamedEvent(m.EventName)
-				if !known {
-					continue
-				}
-				Decode(e, &m)
-				mlog.Log("got ", e)
+			writePrompt()
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			if err := scanner.Err(); err != nil {
+				mlog.Log(err)
+				os.Exit(1)
 			}
+			playerInput <- scanner.Text()
 		}
 	}()
 
+	var (
+		fg     = vt100.ForegroundColors()
+		yellow = fg.Yellow.Bytes()
+		cyan   = fg.Cyan.Bytes()
+
+		vt    = vt100.Attributes()
+		reset = vt.Reset.Bytes()
+	)
+
+	// handle incoming messages
 	for {
-		fmt.Printf("%s> ", cid)
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		err := scanner.Err()
-		if err != nil {
-			mlog.Log(err)
-			os.Exit(1)
-		}
-
-		input := scanner.Text()
-		switch input {
-		case "n", "w", "s", "e":
-			mv := MoveCharacter(cid, nav[input])
-			c.Out <- NewMessage(mv)
-
-		case "l":
-			// todo first position
-			if m.Tile != nil {
-				os.Stdout.Write([]byte(m.Tile.Long))
-				fmt.Println()
+		select {
+		case m := <-c.In:
+			e, known := NewNamedEvent(m.EventName)
+			if !known {
+				continue
 			}
-		case "h", "help":
-			os.Stdout.Write(usage)
-		case "q":
-			c.Close()
-			fmt.Println("bye")
-			os.Exit(0)
-		default:
-			e := &EventSay{Ident: cid, Text: input}
-			c.Out <- NewMessage(e)
-		}
-		if err != nil {
-			fmt.Println(err)
+			Decode(e, &m)
+			switch e := e.(type) {
+			case *EventSay:
+				fmt.Printf("\n%s%s: %s%s\n", cyan, e.Ident, e.Text, reset)
+			default:
+				fmt.Printf("\n%s%v%s\n", yellow, e, reset)
+			}
+			writePrompt()
+
+		case input := <-playerInput:
+			switch input {
+			case "n", "w", "s", "e":
+				mv := MoveCharacter(cid, nav[input])
+				c.Out <- NewMessage(mv)
+
+			case "l":
+				// todo first position
+				if m.Tile != nil {
+					os.Stdout.Write([]byte(m.Tile.Long))
+					fmt.Println()
+				}
+			case "h", "help":
+				os.Stdout.Write(usage)
+			case "q":
+				c.Close()
+				fmt.Println("bye")
+				os.Exit(0)
+			default:
+				e := &EventSay{Ident: cid, Text: input}
+				c.Out <- NewMessage(e)
+			}
+
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -8,3 +8,5 @@ require (
 	github.com/gregoryv/logger v0.2.0
 	github.com/gregoryv/nexus v0.5.1
 )
+
+require github.com/gregoryv/vt100 v0.2.0 // indirect
