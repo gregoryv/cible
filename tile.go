ca6483c3388dd7687330547985975aa315566507
Gregory Vincic <g@7de.se>
2022-05-11 21:05:29 +0200

    Move type Tile

---
+++ tile.go
@@ -0,0 +1,36 @@
+package cible
+
+import "fmt"
+
+type Tile struct {
+	Ident
+	Short
+	Long
+	Nav
+}
+
+func (t *Tile) String() string {
+	return fmt.Sprintf("%s %s", t.Ident, t.Short)
+}
+
+// Link creates a dual link between a tile and the given ones
+func (me *Tile) Link(to ...interface{}) {
+	for i := 0; i < len(to); i += 2 {
+		t := to[i].(*Tile)
+		d := to[i+1].(Direction)
+		if me.Nav[d] != "" {
+			if me.Nav[d] == t.Ident {
+				continue // already linked
+			}
+			panic(
+				fmt.Sprintf(
+					"cannot link %s, %s already linked to %v",
+					me.String(), d.String(), me.Nav[d],
+				),
+			)
+		}
+		// link in both directions
+		me.Nav[d] = t.Ident
+		t.Nav[opposite[d]] = me.Ident
+	}
+}
--- world.go
+++ world.go
@@ -50,38 +50,6 @@ func (a *Area) AddTile(tiles ...*Tile) {
 
 type Tiles []*Tile
 
-type Tile struct {
-	Ident
-	Short
-	Long
-	Nav
-}
-
-func (t *Tile) String() string {
-	return fmt.Sprintf("%s %s", t.Ident, t.Short)
-}
-
-func (me *Tile) Link(to ...interface{}) {
-	for i := 0; i < len(to); i += 2 {
-		t := to[i].(*Tile)
-		d := to[i+1].(Direction)
-		if me.Nav[d] != "" {
-			if me.Nav[d] == t.Ident {
-				continue // already linked
-			}
-			panic(
-				fmt.Sprintf(
-					"cannot link %s, %s already linked to %v",
-					me.String(), d.String(), me.Nav[d],
-				),
-			)
-		}
-		// link in both directions
-		me.Nav[d] = t.Ident
-		t.Nav[opposite[d]] = me.Ident
-	}
-}
-
 // Nav references tiles based on direction Direction -> Tile.Ident
 type Nav [8]Ident
 
